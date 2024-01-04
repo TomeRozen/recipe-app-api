6efaf6fdc04879ea376d8657fad032b2cfe34c90
tomerozen <tomer.rozenstine@cs.huji.ac.il>
2024-01-04 09:57:11 +0200

    Adding basic unit tests and functionality to examine tdd

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calc app
+"""
+
+def add(x, y):
+    """Add two numbers"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract two numbers"""
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,24 @@
+"""
+sample test file
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """
+    sample test class
+    """
+    def test_add_numbers(self):
+        """
+        test adding numbers
+        """
+        res = calc.add(3, 8)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """
+        test subtracting numbers
+        """
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
