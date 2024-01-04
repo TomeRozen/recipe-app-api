44c5f64ac19ac46d7ab38d74af38e3345d06cda4
tomerozen <tomer.rozenstine@cs.huji.ac.il>
2024-01-04 11:19:42 +0200

    fixing the cacl app

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,21 +2,12 @@
 Calc app
 """
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 33868b0 (Adding basic unit tests and functionality to examine tdd)
 def add(x, y):
     """Add two numbers"""
     return x + y
 
-<<<<<<< HEAD
-def subtract(x, y):
-    """Subtract two numbers"""
-    return y - x
-=======
 
 def subtract(x, y):
     """Subtract two numbers"""
     return y - x
->>>>>>> 33868b0 (Adding basic unit tests and functionality to examine tdd)
